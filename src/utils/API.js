54426f2260e8f0b47ed345e6ccd0b9121ed984c7
Michaelmw17 <Michaelmw17@outlook.com>
2021-01-15 12:52:52 +1100

    Potenial proxy request fix

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/1.3";
+const BASEURL = "https://randomuser.me/api/?results=50&inc=name,phone,picture,email,dob&nat=au";
 
 export default {
             search: function () {
